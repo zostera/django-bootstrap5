6fb02b8aace94f5725921608f063e7d75b238fe7
Dylan Verheul <dylan@dyve.net>
2021-04-18 12:37:36 +0200

    Add test

--- tests/test_bootstrap_field.py
+++ tests/test_bootstrap_field.py
@@ -129,7 +129,28 @@ class BootstrapFieldTest(TestCase):
                 '<div class="django_bootstrap5-req mb-3">'
                 '<label for="id_test" class="form-label">Test</label>'
                 '<textarea class="form-control" cols="40" id="id_test" name="test" placeholder="Test"'
-                ' required rows="10">'
+                ' required rows="10"></textarea>'
+                "</div>"
+            ),
+        )
+
+    def test_textarea_floating(self):
+        """Test field with textarea widget and floating label."""
+
+        class TestForm(forms.Form):
+            test = forms.CharField(widget=forms.Textarea)
+
+        test_form = TestForm()
+        html = render_template_with_bootstrap(
+            '{% bootstrap_field form.test layout="floating" %}', context={"form": test_form}
+        )
+        self.assertHTMLEqual(
+            html,
+            (
+                '<div class="django_bootstrap5-req form-floating mb-3">'
+                '<textarea class="form-control" cols="40" id="id_test" name="test" placeholder="Test"'
+                ' required rows="10"></textarea>'
+                '<label for="id_test" class="form-label">Test</label>'
                 "</div>"
             ),
         )
