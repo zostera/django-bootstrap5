9fc2bb054fa2c0b34d898a4302e6d4ff21716566
Dylan Verheul <dylan@dyve.net>
2021-04-20 06:29:55 +0200

    Refactor test forms

--- tests/test_bootstrap_field.py
+++ tests/test_bootstrap_field.py
@@ -1,6 +1,21 @@
+from django import forms
+
 from django_bootstrap5.exceptions import BootstrapError
 
-from .base import BootstrapTestCase, TestForm, html_39x27
+from .base import BootstrapTestCase, html_39x27
+
+
+class XssTestForm(forms.Form):
+    xss_field = forms.CharField(label='XSS" onmouseover="alert(\'Hello, XSS\')" foo="', max_length=100)
+
+
+class SubjectTestForm(forms.Form):
+    subject = forms.CharField(
+        max_length=100,
+        help_text="my_help_text",
+        required=True,
+        widget=forms.TextInput(attrs={"placeholder": "placeholdertest"}),
+    )
 
 
 class FieldTestCase(BootstrapTestCase):
@@ -9,19 +24,19 @@ class FieldTestCase(BootstrapTestCase):
             self.render("{% bootstrap_field field %}", {"field": "illegal"})
 
     def test_show_help(self):
-        html = self.render("{% bootstrap_field form.subject %}", {"form": TestForm()})
+        html = self.render("{% bootstrap_field form.subject %}", {"form": SubjectTestForm()})
         self.assertIn("my_help_text", html)
         self.assertNotIn("<i>my_help_text</i>", html)
-        html = self.render("{% bootstrap_field form.subject show_help=False %}", {"form": TestForm()})
+        html = self.render("{% bootstrap_field form.subject show_help=False %}", {"form": SubjectTestForm()})
         self.assertNotIn("my_help_text", html)
 
     def test_placeholder(self):
-        html = self.render("{% bootstrap_field form.subject %}", {"form": TestForm()})
+        html = self.render("{% bootstrap_field form.subject %}", {"form": SubjectTestForm()})
         self.assertIn('type="text"', html)
         self.assertIn('placeholder="placeholdertest"', html)
 
     def test_xss_field(self):
-        html = self.render("{% bootstrap_field form.xss_field %}", {"form": TestForm()})
+        html = self.render("{% bootstrap_field form.xss_field %}", {"form": XssTestForm()})
         self.assertIn('type="text"', html)
         self.assertIn(html_39x27(">XSS&quot; onmouseover=&quot;alert(&#x27;Hello, XSS&#x27;)&quot; foo=&quot;<"), html)
         self.assertIn(
@@ -30,7 +45,7 @@ class FieldTestCase(BootstrapTestCase):
 
     def test_empty_permitted(self):
         """If a form has empty_permitted, no fields should get the CSS class for required."""
-        form = TestForm()
+        form = SubjectTestForm()
 
         html = self.render("{% bootstrap_field form.subject %}", {"form": form})
         self.assertIn("django_bootstrap5-req", html)
@@ -41,11 +56,11 @@ class FieldTestCase(BootstrapTestCase):
 
     def test_size(self):
         def _test_size(param, klass):
-            html = self.render('{% bootstrap_field form.subject size="' + param + '" %}', {"form": TestForm()})
+            html = self.render('{% bootstrap_field form.subject size="' + param + '" %}', {"form": SubjectTestForm()})
             self.assertIn(klass, html)
 
         def _test_size_medium(param):
-            html = self.render('{% bootstrap_field form.subject size="' + param + '" %}', {"form": TestForm()})
+            html = self.render('{% bootstrap_field form.subject size="' + param + '" %}', {"form": SubjectTestForm()})
             self.assertNotIn("form-control-lg", html)
             self.assertNotIn("form-control-sm", html)
             self.assertNotIn("form-control-md", html)
