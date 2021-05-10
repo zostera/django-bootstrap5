e10011f864ba0aebae2ae37709a1b126c3fd1af8
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2021-05-10 20:09:23 +0200

    Ignore placeholders in attributes in render_tag (#103)
    fixes #102

--- tests/test_html.py
+++ tests/test_html.py
@@ -8,3 +8,4 @@ class HtmlTestCase(TestCase):
         self.assertEqual(render_tag("span"), "<span></span>")
         self.assertEqual(render_tag("span", content="foo"), "<span>foo</span>")
         self.assertEqual(render_tag("span", attrs={"bar": 123}, content="foo"), '<span bar="123">foo</span>')
+        self.assertEqual(render_tag("span", attrs={"bar": "{foo}"}, content="foo"), '<span bar="{foo}">foo</span>')
