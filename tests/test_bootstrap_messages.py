1138185a83b05261bd7831524b782244ccbf8007
Dylan Verheul <dylan@zostera.nl>
2021-07-13 05:53:47 +0200

    Use Django Message class in bootstrap_messages tests (#144)

--- tests/test_bootstrap_messages.py
+++ tests/test_bootstrap_messages.py
@@ -1,22 +1,7 @@
 from django.contrib.messages import constants as DEFAULT_MESSAGE_LEVELS
+from django.contrib.messages.storage.base import Message
 
 from tests.base import BootstrapTestCase
-
-
-class MockMessage(object):
-    """Follows the `django.contrib.messages.storage.base.Message` API."""
-
-    level = None
-    message = None
-    extra_tags = None
-
-    def __init__(self, level, message, extra_tags=None):
-        self.level = level
-        self.extra_tags = extra_tags
-        self.message = message
-
-    def __str__(self):
-        return self.message
 
 
 class MessagesTestCase(BootstrapTestCase):
@@ -29,38 +14,38 @@ class MessagesTestCase(BootstrapTestCase):
         )
 
     def test_bootstrap_messages(self):
-        messages = [MockMessage(DEFAULT_MESSAGE_LEVELS.WARNING, "hello")]
+        messages = [Message(DEFAULT_MESSAGE_LEVELS.WARNING, "hello")]
         self.assertHTMLEqual(
             self.render("{% bootstrap_messages messages %}", {"messages": messages}),
             self._html(content="hello", css_class="alert-warning"),
         )
 
-        messages = [MockMessage(DEFAULT_MESSAGE_LEVELS.ERROR, "hello")]
+        messages = [Message(DEFAULT_MESSAGE_LEVELS.ERROR, "hello")]
         self.assertHTMLEqual(
             self.render("{% bootstrap_messages messages %}", {"messages": messages}),
             self._html(content="hello", css_class="alert-danger"),
         )
 
     def test_bootstrap_messages_with_other_levels(self):
-        messages = [MockMessage(None, "hello")]
+        messages = [Message(DEFAULT_MESSAGE_LEVELS.INFO, "hello")]
         self.assertHTMLEqual(
             self.render("{% bootstrap_messages messages %}", {"messages": messages}),
             self._html(content="hello", css_class="alert-info"),
         )
-        messages = [MockMessage(999, "hello")]
+        messages = [Message(999, "hello")]
         self.assertHTMLEqual(
             self.render("{% bootstrap_messages messages %}", {"messages": messages}),
             self._html(content="hello", css_class="alert-info"),
         )
 
     def test_bootstrap_messages_with_other_content(self):
-        messages = [MockMessage(DEFAULT_MESSAGE_LEVELS.ERROR, "hello http://example.com")]
+        messages = [Message(DEFAULT_MESSAGE_LEVELS.ERROR, "hello http://example.com")]
         self.assertHTMLEqual(
             self.render("{% bootstrap_messages messages %}", {"messages": messages}),
             self._html(content="hello http://example.com", css_class="alert-danger"),
         )
 
-        messages = [MockMessage(DEFAULT_MESSAGE_LEVELS.ERROR, "hello\nthere")]
+        messages = [Message(DEFAULT_MESSAGE_LEVELS.ERROR, "hello\nthere")]
         self.assertHTMLEqual(
             self.render("{% bootstrap_messages messages %}", {"messages": messages}),
             self._html(content="hello there", css_class="alert-danger"),
