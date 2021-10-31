4304657072856dce8a29faa8faa4d011e19059c0
Dylan Verheul <dylan@dyve.net>
2021-10-31 18:57:52 +0100

    Remove unused function

--- src/django_bootstrap5/templatetags/django_bootstrap5.py
+++ src/django_bootstrap5/templatetags/django_bootstrap5.py
@@ -61,16 +61,6 @@ def bootstrap_classes(*args):
 
 
 @register.filter
-def bootstrap_trim(value):
-    """
-    Return string value without leading spaces.
-
-    Please consider this filter private, do not use it in your own templates.
-    """
-    return f"{value}".strip()
-
-
-@register.filter
 def bootstrap_message_alert_type(message):
     """Return the alert type for a message, defaults to `info`."""
     try:
