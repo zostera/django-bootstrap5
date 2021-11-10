55de8e1a46bbad83789b71d6fded6dcf0f0c06ff
Peter Farkas <peter.farkas@indaline.com>
2021-11-10 09:34:29 +0100

    Update core.py

--- src/django_bootstrap5/core.py
+++ src/django_bootstrap5/core.py
@@ -10,7 +10,7 @@ BOOTSTRAP5_DEFAULTS = {
         "crossorigin": "anonymous",
     },
     "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.j",
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
         "integrity": "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p",
         "crossorigin": "anonymous",
     },
