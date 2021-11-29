a55a1abcc744bcfbd2cba077fb615539d0c45654
James Hodgkinson <james@terminaloutcomes.com>
2021-11-29 11:12:02 +1000

    refactored test slightly

--- tests/test_urls_valid.py
+++ tests/test_urls_valid.py
@@ -1,18 +1,14 @@
 from base64 import b64decode
 import hashlib
 import logging
-
-from django.test import TestCase
-
-from django_bootstrap5.core import get_bootstrap_setting
 import urllib.request
 
-
+from django.test import TestCase
+from django_bootstrap5.core import get_bootstrap_setting
 
 class UrlValidityTestCase(TestCase):
     def test_get_bootstrap_setting(self):
-        """ Tests that it's possible to pull the URLs listed in the settings, and the hashes match.
-        """
+        """ Tests that it's possible to pull the URLs listed in the settings, and the hashes match. """
 
         logger = logging.getLogger(__name__)
 
@@ -23,16 +19,18 @@ class UrlValidityTestCase(TestCase):
             req = urllib.request.Request(url=link.get("url"), method='GET')
 
             hashtype, hash = link.get("integrity").split("-")
-            hash_decoded = b64decode(hash)
+
+            # Test that it's a valid hash type
             self.assertTrue(hasattr(hashlib, hashtype))
 
             with urllib.request.urlopen(req) as request:
                 logger.debug(f"Status for {link.get('url')}: {request.status}")
-                # test that we're getting a "valid" response code from the specified URL
+
+                # Test that we're getting a "valid" response code from the specified URL
                 self.assertLess(request.status,400)
 
                 # Test that the integrity hash matches
-                hasher = hashlib.new(hashtype)
-                hasher.update(request.read())
-                self.assertEqual(hasher.digest(), hash_decoded)
+                hasher = hashlib.new(hashtype, request.read())
+
+                self.assertEqual(hasher.digest(), b64decode(hash))
 
