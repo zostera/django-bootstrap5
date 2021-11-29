56b7293e8dfd659ff6016f4052d051a7e80ad397
James Hodgkinson <james@terminaloutcomes.com>
2021-11-29 11:06:24 +1000

    updated docstring

--- tests/test_urls_valid.py
+++ tests/test_urls_valid.py
@@ -1,5 +1,6 @@
+from base64 import b64decode
+import hashlib
 import logging
-
 
 from django.test import TestCase
 
@@ -10,10 +11,7 @@ import urllib.request
 
 class UrlValidityTestCase(TestCase):
     def test_get_bootstrap_setting(self):
-        """ tests that it's possible to pull the URLs listed in the settings
-
-        Reason: https://github.com/zostera/django-bootstrap5/issues/208
-
+        """ Tests that it's possible to pull the URLs listed in the settings, and the hashes match.
         """
 
         logger = logging.getLogger(__name__)
@@ -21,10 +19,20 @@ class UrlValidityTestCase(TestCase):
         jsurl = get_bootstrap_setting("javascript_url")
         cssurl = get_bootstrap_setting("css_url")
 
-        for url in (jsurl.get("url"), cssurl.get("url")):
-            req = urllib.request.Request(url=url, method='GET')
+        for link in (jsurl, cssurl):
+            req = urllib.request.Request(url=link.get("url"), method='GET')
+
+            hashtype, hash = link.get("integrity").split("-")
+            hash_decoded = b64decode(hash)
+            self.assertTrue(hasattr(hashlib, hashtype))
 
             with urllib.request.urlopen(req) as request:
-                logger.debug(f"Status for {url}: {request.status}")
+                logger.debug(f"Status for {link.get('url')}: {request.status}")
+                # test that we're getting a "valid" response code from the specified URL
                 self.assertLess(request.status,400)
+
+                # Test that the integrity hash matches
+                hasher = hashlib.new(hashtype)
+                hasher.update(request.read())
+                self.assertEqual(hasher.digest(), hash_decoded)
 
