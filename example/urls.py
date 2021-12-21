7f20f7c19407174f34d002b1777a024dd2ea4824
George Tantiras <raratiru@users.noreply.github.com>
2021-12-21 02:44:44 +0200

    Django-4.0 Compatibility

--- example/urls.py
+++ example/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from app.views import (
     DefaultFormByFieldView,
@@ -13,13 +13,13 @@ from app.views import (
 )
 
 urlpatterns = [
-    url(r"^$", HomePageView.as_view(), name="home"),
-    url(r"^formset$", DefaultFormsetView.as_view(), name="formset_default"),
-    url(r"^form$", DefaultFormView.as_view(), name="form_default"),
-    url(r"^form_by_field$", DefaultFormByFieldView.as_view(), name="form_by_field"),
-    url(r"^form_horizontal$", FormHorizontalView.as_view(), name="form_horizontal"),
-    url(r"^form_inline$", FormInlineView.as_view(), name="form_inline"),
-    url(r"^form_with_files$", FormWithFilesView.as_view(), name="form_with_files"),
-    url(r"^pagination$", PaginationView.as_view(), name="pagination"),
-    url(r"^misc$", MiscView.as_view(), name="misc"),
+    re_path(r"^$", HomePageView.as_view(), name="home"),
+    re_path(r"^formset$", DefaultFormsetView.as_view(), name="formset_default"),
+    re_path(r"^form$", DefaultFormView.as_view(), name="form_default"),
+    re_path(r"^form_by_field$", DefaultFormByFieldView.as_view(), name="form_by_field"),
+    re_path(r"^form_horizontal$", FormHorizontalView.as_view(), name="form_horizontal"),
+    re_path(r"^form_inline$", FormInlineView.as_view(), name="form_inline"),
+    re_path(r"^form_with_files$", FormWithFilesView.as_view(), name="form_with_files"),
+    re_path(r"^pagination$", PaginationView.as_view(), name="pagination"),
+    re_path(r"^misc$", MiscView.as_view(), name="misc"),
 ]
