a084f52d40d3f3e692a670a8c68c97e090b48dfc
Aarni Koskela <akx@iki.fi>
2022-01-20 13:32:54 +0200

    Switch to declarative setup.cfg

--- src/django_bootstrap5/__init__.py
+++ src/django_bootstrap5/__init__.py
@@ -2,12 +2,4 @@ __all__ = [
     "__version__",
 ]
 
-PACKAGE_NAME = "django-bootstrap5"
-
-try:
-    from importlib.metadata import metadata
-except ImportError:
-    from importlib_metadata import metadata
-
-package_metadata = metadata(PACKAGE_NAME)
-__version__ = package_metadata["Version"]
+__version__ = "21.3"
