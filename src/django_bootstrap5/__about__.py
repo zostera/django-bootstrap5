1da4f9b93970185523ed722579853a1ab8a755fc
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2024-02-14 20:29:15 +0100

    Fix autodoc build (fixes #543)

--- src/django_bootstrap5/__about__.py
+++ src/django_bootstrap5/__about__.py
@@ -1,3 +1,6 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__package__ or __name__)
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "dev"
