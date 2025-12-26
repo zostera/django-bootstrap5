f037fd5b98369530800bbd5442c434b422a7a661
Dylan Verheul <dylan@dyve.net>
2025-12-26 10:45:34 +0100

    Fixed one error

--- tests/test_bootstrap_field_datetime.py
+++ tests/test_bootstrap_field_datetime.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.admin.widgets import AdminSplitDateTime
 
-from tests.base import BootstrapTestCase
+from tests.base import DJANGO_VERSION, BootstrapTestCase
 
 
 class DateTimeTestForm(forms.Form):
@@ -11,22 +11,39 @@ class DateTimeTestForm(forms.Form):
 class DateTimeTestCase(BootstrapTestCase):
     def test_input_type_admin_split_date_time(self):
         """Test field with AdminSplitDateTime widget."""
-        expected_html = (
-            '<div class="django_bootstrap5-req mb-3">'
-            '<label class="form-label">Test</label>'
-            '<p class="datetime">'
-            "Date: "
-            '<input type="text" name="test_0" class="form-control vDateField" size="10"'
-            ' placeholder="Test" required id="id_test_0">'
-            "<br>"
-            "Time: "
-            '<input type="text" name="test_1" class="form-control vTimeField" size="8"'
-            ' placeholder="Test" required id="id_test_1">'
-            "</p>"
-            "</div>"
-        )
+        if DJANGO_VERSION >= "6.0":
+            expected_html = (
+                '<div class="mb-3 django_bootstrap5-req">'
+                '<label class="form-label" for="id_test">Test</label>'
+                '<p class="datetime">'
+                '<label for="id_test_0">Date:</label> '
+                ' <input type="text" name="test_0" class="form-control vDateField" size="10"'
+                ' placeholder="Test" required id="id_test_0" aria-describedby="id_test_timezone_warning_helptext">'
+                "<br>"
+                '<label for="id_test_1">Time:</label>'
+                ' <input type="text" name="test_1" class="form-control vTimeField" size="8"'
+                ' placeholder="Test" required id="id_test_1" aria-describedby="id_test_timezone_warning_helptext">'
+                "</p>"
+                "</div>"
+            )
+        else:
+            expected_html = (
+                '<div class="django_bootstrap5-req mb-3">'
+                '<label class="form-label">Test</label>'
+                '<p class="datetime">'
+                "Date: "
+                '<input type="text" name="test_0" class="form-control vDateField" size="10"'
+                ' placeholder="Test" required id="id_test_0">'
+                "<br>"
+                "Time: "
+                '<input type="text" name="test_1" class="form-control vTimeField" size="8"'
+                ' placeholder="Test" required id="id_test_1">'
+                "</p>"
+                "</div>"
+            )
+        html = self.render("{% bootstrap_field form.test %}", context={"form": DateTimeTestForm()})
         self.assertHTMLEqual(
-            self.render("{% bootstrap_field form.test %}", context={"form": DateTimeTestForm()}),
+            html,
             expected_html,
         )
 
@@ -48,11 +65,12 @@ class DateTimeTestCase(BootstrapTestCase):
             "</div>"
             "</div>"
         )
+        html = self.render(
+            '{% bootstrap_field form.test layout="horizontal" %}',
+            context={"form": DateTimeTestForm()},
+        )
         self.assertHTMLEqual(
-            self.render(
-                '{% bootstrap_field form.test layout="horizontal" %}',
-                context={"form": DateTimeTestForm()},
-            ),
+            html,
             expected_html,
         )
 
@@ -60,22 +78,23 @@ class DateTimeTestCase(BootstrapTestCase):
         """Test field with AdminSplitDateTime widget with layout floating."""
         expected_html = (
             '<div class="django_bootstrap5-req mb-3">'
-            '<label class="form-label">Test</label>'
-            '<p class="datetime">'
-            "Date: "
+            '<div class="form-floating">'
             '<input type="text" name="test_0" class="form-control vDateField" size="10"'
             ' placeholder="Test" required id="id_test_0">'
-            "<br>"
-            "Time: "
+            '<label for="id_test_0">Test - Date:</label>'
+            "</div>"
+            '<div class="form-floating mt-2">'
             '<input type="text" name="test_1" class="form-control vTimeField" size="8"'
             ' placeholder="Test" required id="id_test_1">'
-            "</p>"
+            '<label for="id_test_1">Test - Time:</label>'
+            "</div>"
             "</div>"
         )
+        html = self.render(
+            '{% bootstrap_field form.test layout="floating" %}',
+            context={"form": DateTimeTestForm()},
+        )
         self.assertHTMLEqual(
-            self.render(
-                '{% bootstrap_field form.test layout="floating" %}',
-                context={"form": DateTimeTestForm()},
-            ),
+            html,
             expected_html,
         )
